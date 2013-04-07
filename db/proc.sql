40f007f7e85b4550da849b4740129f7029a7e446
Chandan Rai <chandanrai2@gmail.com>
2013-04-07 17:02:54 +1000

    Added DB schema files

---
+++ db/proc.sql
@@ -0,0 +1,15 @@
+create or replace procedure delete_route(route_id varchar2) as
+begin
+  delete from route where route_id = route_id;
+end;
+/
+
+create or replace procedure delete_airport(airpt_id varchar2) as
+child_exists  exception;
+pragma exception_init (child_exists , -02292);
+begin
+  delete from airport where airpt_id = airpt_id;
+exception
+   when child_exists  then
+      raise_application_error(-20001, ': Delete airport failed. '); 
+end;
