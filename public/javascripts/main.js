afa7f9199ee6baeec8520bb8606088d7f36e939d
Chandan Rai <chandanrai2@gmail.com>
2013-04-07 16:26:22 +1000

    Changes done for modal.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     var parts = document.location.pathname.split("/");
     $("#" + parts[1]).addClass("active");
+    $(".delete-link").click(function(e){
+        e.preventDefault();
+        $("#delete").attr("href", $(this).attr("path"));
+   });
 });
