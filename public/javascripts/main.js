dd32cbed1246d92c2cfe158750fcb8befc6ba6db
Prakriti <itirkarp@gmail.com>
2013-04-08 01:15:09 +1000

    UI fancyness. show and hide route segments in the route view

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,11 +7,26 @@ $(document).ready(function(){
         $("#delete").attr("href", $(this).attr("path"));
     });
     
+    bindShowSegment();
+    
     copyRouteForm();
-   
+    
     fillRouteSegmentFormOnChange();
-   
+
 });
+
+function bindShowSegment() {
+    $('a[id$="show-segment"]').click(function(){
+        var element = "#" + $(this).attr("id").split("-")[0]+"-segment";
+        if ($(element).hasClass("hide")) {
+            $(element).removeClass("hide");
+            $(this).html("<i class=\"icon-arrow-up\"></i>Hide Segments");
+        } else {
+            $(element).addClass("hide");
+            $(this).html("<i class=\"icon-arrow-down\"></i>Show Segments");
+        }
+    });
+}
 
 function fillRouteSegmentFormOnChange() {
     $("#airpt_id_from").change(function(){
@@ -32,6 +47,6 @@ function copyRouteForm() {
     $("#segment\\.airpt_id_from").val($("#airpt_id_from").val());
     $("#segment\\.airpt_id_to").val($("#airpt_id_to").val());
     $("#segment\\.dep_time").val($("#dep_time").val());
-
+    
     $("#segment\\.arr_time").val($("#arr_time").val());
 }
