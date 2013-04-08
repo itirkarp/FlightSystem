901988d77a33cdc0fd30873eab9f4a227a6264b5
Chandan Rai <chandanrai2@gmail.com>
2013-04-08 19:03:52 +1000

    Error handling route edit.

--- app/controllers/RouteController.java
+++ app/controllers/RouteController.java
@@ -13,15 +13,15 @@ import views.html.*;
 public class RouteController extends Controller {
 
     static Form<Route> routeForm = Form.form(Route.class);
-    static final HashMap<String, String> errorMessages = new HashMap<String, String>(){
+    static final HashMap<String, String> errorMessages = new HashMap<String, String>() {
         {
-            put("ROUT_PK", "Cannot create route. This route already exists");
-            put("ROUT_FROM", "Cannot create route. The source airport does not exist");
-            put("ROUT_TO", "Cannot create route. The destination airport does not exist");
-            put("ROUT_OPERATED_BY", "Cannot create route. The airline does not exist");
+            put("ROUT_PK", "Cannot save route. This route already exists");
+            put("ROUT_FROM", "Cannot save route. The source airport does not exist");
+            put("ROUT_TO", "Cannot save route. The destination airport does not exist");
+            put("ROUT_OPERATED_BY", "Cannot save route. The airline does not exist");
         }
-    };    
-    
+    };
+
     public static Result index() {
         return ok(route_index.render(Route.all()));
     }
@@ -29,7 +29,7 @@ public class RouteController extends Controller {
     public static Result create() {
         return ok(route_create.render(routeForm));
     }
-    
+
     public static Result edit(String id) {
         Route route = Route.find.ref(id);
         return ok(route_edit.render(route, routeForm.fill(route)));
@@ -43,8 +43,21 @@ public class RouteController extends Controller {
             return badRequest(route_edit.render(route, filledForm));
         } else {
             Route route = filledForm.get();
-            Route.update(route.route_id, route.arr_time, route.airpt_id_to, 
-                    route.dep_time, route.airpt_id_from, route.airln_id, route.day_no);
+            try {
+                
+                Route.update(route.route_id, route.arr_time, route.airpt_id_to,
+                        route.dep_time, route.airpt_id_from, route.airln_id, route.day_no);
+            } catch (PersistenceException e) {
+                String[] temp = e.getMessage().split("S1784498.");
+                if (temp.length > 1) {
+                    temp = temp[1].split("\\) violated");
+                    String constraintName = temp[0];
+                    flash("error", errorMessages.get(constraintName));
+                } else {
+                    flash("error", "Cannot create route. A database error occurred: " + e.getMessage());
+                }
+                return badRequest(route_edit.render(route, filledForm));
+            }
         }
         return ok(route_index.render(Route.all()));
     }
@@ -53,7 +66,7 @@ public class RouteController extends Controller {
         Route.find.ref(id).delete();
         return ok(route_index.render(Route.all()));
     }
-    
+
     public static Result save() {
         Form<Route> filledForm = routeForm.bindFromRequest();
         if (filledForm.hasErrors()) {
@@ -67,9 +80,9 @@ public class RouteController extends Controller {
                 route.overbook_i = 0;
                 Route.create(route);
                 RouteSegment.create(route.arr_time, route.dep_time, route.airpt_id_to, route.airpt_id_from, route);
-            } catch(PersistenceException e) {
+            } catch (PersistenceException e) {
                 String[] temp = e.getMessage().split("S1784498.");
-                if (temp.length >1 ) {
+                if (temp.length > 1) {
                     temp = temp[1].split("\\) violated");
                     String constraintName = temp[0];
                     flash("error", errorMessages.get(constraintName));
@@ -81,5 +94,4 @@ public class RouteController extends Controller {
         }
         return ok(route_index.render(Route.all()));
     }
-    
 }
