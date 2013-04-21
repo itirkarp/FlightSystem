83f0981ceb96c22f69a62a9b34a672f4bcc84878
Prakriti <itirkarp@gmail.com>
2013-04-22 04:05:43 +1000

    basic route segment validations

--- app/controllers/RouteController.java
+++ app/controllers/RouteController.java
@@ -8,7 +8,6 @@ import models.Airline;
 import models.Airport;
 import models.Route;
 import models.RouteSegment;
-import play.Logger;
 import play.data.Form;
 import play.data.validation.ValidationError;
 import play.mvc.*;
@@ -16,10 +15,6 @@ import views.html.route.*;
 
 public class RouteController extends Controller {
 
-    static ArrayList<ValidationError> errorList = new ArrayList<ValidationError>() {{
-            add(new ValidationError("Source and destination airports should be different.", 
-                                    "Source and destination airports should be different."));
-        }};
     static Form<Route> routeForm = Form.form(Route.class);
     static final HashMap<String, String> errorMessages = new HashMap<String, String>() {
         {
@@ -48,33 +43,40 @@ public class RouteController extends Controller {
     public static Result update(String id) {
         Form<Route> filledForm = routeForm.bindFromRequest();
         Route route = Route.find.ref(id);
+        Route newRoute = filledForm.get();
+        
         if (filledForm.hasErrors()) {
             flash("error", "There were errors in the form:");
             return badRequest(route_edit.render(route, filledForm, Airline.all(), Airport.all()));
-        } else if (filledForm.get().areAirportsSame()) {
-            filledForm.errors().put("airpt_id_from", errorList);
-            flash("error", "There were errors in the form:");
-            return badRequest(route_edit.render(route, filledForm, Airline.all(), Airport.all()));
-        } else {
-            Route newRoute = filledForm.get();
-            try {
-                Route.update(route.route_id, newRoute.arr_time, newRoute.airpt_id_to,
-                        newRoute.dep_time, newRoute.airpt_id_from, newRoute.airln_id, newRoute.day_no);
-                for (RouteSegment segment : newRoute.segments) {
-                    RouteSegment.update(segment.seg_no, segment.arr_time, segment.dep_time, segment.airpt_id_to, segment.airpt_id_from);
-                }
-            } catch (PersistenceException e) {
-                String[] temp = e.getMessage().split("S1784498.");
-                if (temp.length > 1) {
-                    temp = temp[1].split("\\) violated");
-                    String constraintName = temp[0];
-                    flash("error", errorMessages.get(constraintName));
-                } else {
-                    flash("error", "Cannot create route. A database error occurred: " + e.getMessage());
-                }
-                return badRequest(route_edit.render(newRoute, filledForm, Airline.all(), Airport.all()));
-            }
+        } 
+        if (newRoute.areAirportsSame()) {
+            return redirect_error(filledForm, "airpt_id_from", "Source and destination airports should be different.");
+        } 
+        if (!newRoute.airpt_id_from.equals(newRoute.segments.get(0).airpt_id_from)){
+            return redirect_error(filledForm, "airpt_id_from", "Source airport of the route must match the source airport of the first segment.");
         }
+        if (!newRoute.airpt_id_to.equals(newRoute.segments.get(newRoute.segments.size() - 1).airpt_id_to)){
+            return redirect_error(filledForm, "airpt_id_to", "Destination airport of the route must match the destination airport of the last segment.");
+        }
+        
+        try {
+            Route.update(route.route_id, newRoute.arr_time, newRoute.airpt_id_to,
+                    newRoute.dep_time, newRoute.airpt_id_from, newRoute.airln_id, newRoute.day_no);
+            for (RouteSegment segment : newRoute.segments) {
+                RouteSegment.update(segment.seg_no, segment.arr_time, segment.dep_time, segment.airpt_id_to, segment.airpt_id_from);
+            }
+        } catch (PersistenceException e) {
+            String[] temp = e.getMessage().split("S1784498.");
+            if (temp.length > 1) {
+                temp = temp[1].split("\\) violated");
+                String constraintName = temp[0];
+                flash("error", errorMessages.get(constraintName));
+            } else {
+                flash("error", "Cannot create route. A database error occurred: " + e.getMessage());
+            }
+            return badRequest(route_edit.render(newRoute, filledForm, Airline.all(), Airport.all()));
+        }
+        
         return ok(route_index.render(Route.all()));
     }
 
@@ -85,36 +87,51 @@ public class RouteController extends Controller {
 
     public static Result save() {
         Form<Route> filledForm = routeForm.bindFromRequest();
+        Route route = filledForm.get();
+        
         if (filledForm.hasErrors()) {
             flash("error", "There were errors in the form:");
             return badRequest(route_create.render(filledForm, Airline.all(), Airport.all()));
-        } else if (filledForm.get().areAirportsSame()) {
-            filledForm.errors().put("airpt_id_from", errorList);
-            flash("error", "There were errors in the form:");
-            return badRequest(route_create.render(filledForm, Airline.all(), Airport.all()));
-        } else {
-            Ebean.beginTransaction();
-            try {
-                Route route = filledForm.get();
-                Route.create(route);
-                for (RouteSegment segment : route.segments) {
-                    RouteSegment.create(segment.arr_time, segment.dep_time, segment.airpt_id_to, segment.airpt_id_from, route);
-                }
-                Ebean.commitTransaction();
-            } catch (PersistenceException e) {
-                flash("error", "Cannot create route. A database error occurred: " + e.getMessage());
-                String[] temp = e.getMessage().split("S1784498.");
-                if (temp.length > 1) {
-                    temp = temp[1].split("\\) violated");
-                    String constraintName = temp[0];
-                    flash("error", errorMessages.get(constraintName));
-                }
-                Ebean.rollbackTransaction();
-                return badRequest(route_create.render(filledForm, Airline.all(), Airport.all()));
-            } finally {
-                Ebean.endTransaction();  
-            }
+        } 
+        if (route.areAirportsSame()) {
+            return redirect_error(filledForm, "airpt_id_from", "Source and destination airports should be different.");
         }
+        if (!route.airpt_id_from.equals(route.segments.get(0).airpt_id_from)){
+            return redirect_error(filledForm, "airpt_id_from", "Source airport of the route must match the source airport of the first segment.");
+        }
+        if (!route.airpt_id_to.equals(route.segments.get(route.segments.size() - 1).airpt_id_to)){
+            return redirect_error(filledForm, "airpt_id_to", "Destination airport of the route must match the destination airport of the last segment.");
+        }
+        
+        Ebean.beginTransaction();
+        try {
+            Route.create(route);
+            for (RouteSegment segment : route.segments) {
+                RouteSegment.create(segment.arr_time, segment.dep_time, segment.airpt_id_to, segment.airpt_id_from, route);
+            }
+            Ebean.commitTransaction();
+        } catch (PersistenceException e) {
+            flash("error", "Cannot create route. A database error occurred: " + e.getMessage());
+            String[] temp = e.getMessage().split("S1784498.");
+            if (temp.length > 1) {
+                temp = temp[1].split("\\) violated");
+                String constraintName = temp[0];
+                flash("error", errorMessages.get(constraintName));
+            }
+            Ebean.rollbackTransaction();
+            return badRequest(route_create.render(filledForm, Airline.all(), Airport.all()));
+        } finally {
+            Ebean.endTransaction();  
+        }
+        
         return ok(route_index.render(Route.all()));
+    }
+
+    private static Result redirect_error(Form<Route> filledForm, String field, String error) {
+        ArrayList<ValidationError> errorList = new ArrayList<ValidationError>();
+        errorList.add(new ValidationError("", error));
+        filledForm.errors().put(field, errorList);
+        flash("error", "There were errors in the form:");
+        return badRequest(route_create.render(filledForm, Airline.all(), Airport.all()));
     }
 }
