8e3f4b57a458dc344269bcb0140155cc5ca915ad
Chandan Rai <chandanrai2@gmail.com>
2013-05-26 12:42:23 +1000

    Remove date clause from flight search criteria and redirecting on booking page after ticket delete.

--- app/controllers/BookingController.java
+++ app/controllers/BookingController.java
@@ -54,9 +54,9 @@ public class BookingController extends Controller {
     
     public static Result flights() {
         DynamicForm form = DynamicForm.form().bindFromRequest();
-        List<SqlRow> rows = Ebean.createSqlQuery("select unique f.flight_id, f.route_id, f.dep_date, f.dep_time, f.arr_time, s.seats_avail - s.seats_booked as seats from route r, flight f,  flight_seg seg, seats_avail s where r.route_id = f.route_id and f.flight_id = seg.flight_id and seg.seg_no = s.seg_no and r.route_id = s.route_id and r.airpt_id_from = :origin and r.airpt_id_to = :destination and class_id = :class and f.dep_date = :date")
+        List<SqlRow> rows = Ebean.createSqlQuery("select unique f.flight_id, f.route_id, f.dep_date, f.dep_time, f.arr_time, s.seats_avail - s.seats_booked as seats from route r, flight f,  flight_seg seg, seats_avail s where r.route_id = f.route_id and f.flight_id = seg.flight_id and seg.seg_no = s.seg_no and r.route_id = s.route_id and r.airpt_id_from = :origin and r.airpt_id_to = :destination and class_id = :class")
                             .setParameter("origin", form.get("origin")).setParameter("destination", form.get("destination"))
-                            .setParameter("class", form.get("class_id")).setParameter("date", form.get("date")).findList();
+                            .setParameter("class", form.get("class_id")).findList();
         if (rows.isEmpty()) {
             return ok(no_flights.render());
         }
@@ -86,8 +86,7 @@ public class BookingController extends Controller {
         } catch (Exception e) {
             //flash("error", errorMessages.get(e.getMessage().substring(0, 9)));
         }
-//        return view(id);
-        return ok();
+        return ok(booking_index.render(Ticket.all()));
     }
 
 }
